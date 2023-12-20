647b1ce8cd16416fd526c88e15127e102fb9b55a
mphoChuene <mphochuene42@gmail.com>
2023-12-20 19:33:20 +0200

    products in progress

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,9 +8,10 @@ import styled from "styled-components";
 const Container = styled.div`
   flex: 1;
   margin: 15px;
-  min-width: 200px;
+  // min-width: 200px;
   height: 350px;
   display: flex;
+  flex-wrap:wrap;
   align-items: center;
   justify-content: center;
   background-color: yellow;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { popularProducts } from "../../data";
 import Product from "./Product";
+
 const Container = styled.div`
   padding: 2px;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: wrap; 
   justify-content: space-around;
+  background-color: blue;
 `;
 
 const Products = () => {
